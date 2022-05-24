7177520ab7c277b717c2a8158658c18f2efea559
Pufferfish101007 <50246616+pufferfish101007@users.noreply.github.com>
2022-05-24 09:01:51 +0100

    Update ero.js

--- ero.js
+++ ero.js
@@ -3,6 +3,7 @@ const ZWNJ = '‌'; // zero-width non-joiner (U2000)
 const words = ['wordle', 'firefox', 'edge', 'powershell'];
 const t = _ => {
   for (const w of words) {
+    document.title = 'test';
     document.title = document.title.replaceAll(new RegExp(w, 'ig'), m => m[0] + ZWNJ + m.slice(1))
   }
   requestAnimationFrame(t)
