556a03ea5fc0a32c69fb34dc5461b3c299181238
Pufferfish101007 <50246616+pufferfish101007@users.noreply.github.com>
2022-05-24 08:57:13 +0100

    Create ero.js

---
+++ ero.js
@@ -0,0 +1,10 @@
+console.log('ERO v1.0.0');
+const ZWNJ = '‌'; // zero-width non-joiner (U2000)
+const words = ['wordle', 'firefox', 'edge', 'powershell'];
+const t = _ => {
+  for (const w of words) {
+    document.title = document.title.replaceAll(new RegExp(w, 'ig'), m => m[0] + ZWNJ + m.slice(1))
+  }
+  requestAnimationFrame(t)
+}
+requestAnimationFrame(t);
